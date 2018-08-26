647e1762d04f32432a8196abf75d4868b9a07d0e
Claudio Guerra <claudevandort@gmail.com>
2018-08-26 08:35:12 -0300

    simple list view

---
+++ Lists/app/src/main/java/com/example/claudevandort/lists/ListViewActivity.java
@@ -0,0 +1,43 @@
+package com.example.claudevandort.lists;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayList<String> names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        listView = findViewById(R.id.list_view);
+
+        names = new ArrayList<>();
+        names.add("Claudio");
+        names.add("Abel");
+        names.add("Alvaro");
+        names.add("Nico");
+        names.add("Joe");
+        names.add("Max");
+        names.add("Roderick");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(ListViewActivity.this, android.R.layout.simple_list_item_1, names);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewActivity.this, "Clicked: " + names.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
--- Lists/app/src/main/java/com/example/claudevandort/lists/MainActivity.java
+++ Lists/app/src/main/java/com/example/claudevandort/lists/MainActivity.java
@@ -1,33 +1,43 @@
 package com.example.claudevandort.lists;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView listView;
+    private ListView mainListView;
+    private ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.list_view);
+        mainListView = findViewById(R.id.main_list_view);
 
-        ArrayList<String> names = new ArrayList<>();
-        names.add("Claudio");
-        names.add("Abel");
-        names.add("Alvaro");
-        names.add("Nico");
-        names.add("Joe");
-        names.add("Max");
-        names.add("Roderick");
+        items = new ArrayList<>();
+        items.add("ListView");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, items);
 
-        listView.setAdapter(adapter);
+        mainListView.setAdapter(adapter);
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent();
+                switch (position){
+                    case 0:
+                        i.setClass(MainActivity.this, ListViewActivity.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
     }
 }
