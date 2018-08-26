85ce4354ed396e8514e3d0bb2321fda5ce223286
Claudio Guerra <claudevandort@gmail.com>
2018-08-26 17:27:05 -0300

    rectcler view: add/delete items

--- Lists/app/src/main/java/com/example/claudevandort/lists/RecyclerViewActivity.java
+++ Lists/app/src/main/java/com/example/claudevandort/lists/RecyclerViewActivity.java
@@ -2,8 +2,11 @@ package com.example.claudevandort.lists;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -35,11 +38,43 @@ public class RecyclerViewActivity extends AppCompatActivity {
         mAdapter = new MyRecyclerAdapter(names, R.layout.recycler_view_item, new MyRecyclerAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(String name, int position) {
-                Toast.makeText(RecyclerViewActivity.this, "Clicked: " + name, Toast.LENGTH_SHORT).show();
+                Toast.makeText(RecyclerViewActivity.this, "Deleted " + name, Toast.LENGTH_SHORT).show();
+                deleteName(position);
             }
         });
 
+        mRecyclerview.hasFixedSize(true);
+        mRecyclerview.setItemAnimator(new DefaultItemAnimator());
         mRecyclerview.setLayoutManager(mLayoutManager);
         mRecyclerview.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item_menu_option:
+                this.addName(0);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    private void addName(int position){
+        names.add(position, "New item " + (names.size()+1));
+        mAdapter.notifyItemInserted(position);
+        mLayoutManager.scrollToPosition(position);
+    }
+
+    private void deleteName(int position){
+        names.remove(position);
+        mAdapter.notifyItemRemoved(position);
     }
 }
