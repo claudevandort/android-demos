67ccff8bb499394e6f3ddeaab09da54199e40367
Claudio Guerra <claudevandort@gmail.com>
2018-08-28 16:44:10 -0300

    Realm: models

---
+++ RealmNotes/app/src/main/java/com/example/claudevandort/realmnotes/activities/BoardActivity.java
@@ -0,0 +1,15 @@
+package com.example.claudevandort.realmnotes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.claudevandort.realmnotes.R;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
---
+++ RealmNotes/app/src/main/java/com/example/claudevandort/realmnotes/models/Board.java
@@ -0,0 +1,57 @@
+package com.example.claudevandort.realmnotes.models;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Board extends RealmObject {
+    @PrimaryKey
+    private int id;
+    @Required
+    private String title;
+    @Required
+    private Date createdAt;
+    private RealmList<Note> notes;
+
+    public Board(String title) {
+        this.id = 0;
+        this.title = title;
+        this.createdAt = new Date();
+        this.notes = new RealmList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public RealmList<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(RealmList<Note> notes) {
+        this.notes = notes;
+    }
+}
